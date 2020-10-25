6e12b2e0905e32dd1f7b4e3d1b6a6815be9c8afa
konieczn <danieltchemerinskij@gmail.com>
2020-10-25 19:35:11 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A function that makes a special "matrix" object that can cache its inverse.
+## Then a function that figures out the inverse of the "matrix" returned by the above.
+## Yet it if has already computed this (and has not changed), it should instead retrieve it from
+## the cache.
 
-## Write a short comment describing this function
-
+## Part one: Creating a special "matrix" object that can cache its inverse.
+## OBS assumed it's invertible. 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL ##assigning a value to a an object in an enviroment different from current) 
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function () m
+        list (set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Part 2: Return a matrix that is the inverse of x taking care to notice whether it was computed before.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        m <- x$getinverse()
+        if(!is.null(m)){
+                message("Getting cached data.")
+                return(m)
+        }
+        else
+                data <- x$get() ##get data of matrix
+                m <- solve(data, ...) ## compute
+                x$setinverse(m) ## store this in inverse
+                m ## also print it
+        
 }
